5ec2ad0c416142245a6d93ea3203d7bf28928481
Victor <Victor.Tenbergen@capgemini.com>
2017-10-17 09:49:01 +0200

    First commit with Hello World - test

--- src/main/java/com/capgemini/LMDBforAssessment/LmdBforAssessmentApplication.java
+++ src/main/java/com/capgemini/LMDBforAssessment/LmdBforAssessmentApplication.java
@@ -8,5 +8,6 @@ public class LmdBforAssessmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmdBforAssessmentApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
