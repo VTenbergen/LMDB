e794101e001f756f344ccb5248d06014186f2557
Victor <Victor.Tenbergen@capgemini.com>
2017-10-17 09:47:11 +0200

    First commit

---
+++ src/main/java/com/capgemini/LMDBforAssessment/LmdBforAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.LMDBforAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmdBforAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmdBforAssessmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/LMDBforAssessment/LmdBforAssessmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.LMDBforAssessment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LmdBforAssessmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
