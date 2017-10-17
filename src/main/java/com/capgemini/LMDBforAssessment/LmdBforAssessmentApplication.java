7e19c9f3d9faf6df7a95990f769f309eafeb322c
Victor <Victor.Tenbergen@capgemini.com>
2017-10-17 09:55:06 +0200

    Commit with updated .gitignore

--- src/main/java/com/capgemini/LMDBforAssessment/LmdBforAssessmentApplication.java
+++ src/main/java/com/capgemini/LMDBforAssessment/LmdBforAssessmentApplication.java
@@ -8,6 +8,5 @@ public class LmdBforAssessmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmdBforAssessmentApplication.class, args);
-		System.out.println("Hello World");
 	}
 }
